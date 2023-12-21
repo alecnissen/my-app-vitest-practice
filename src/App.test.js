e4432a53ca9b6e9601ad64ece770b8894bc925e3
alecnissen <alecnissen@yahoo.com>
2023-12-20 23:37:20 -0500

    first test passed, now working on multiple testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
+import React from 'react'
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('render h1 element', () => { 
+  render (<App />);
+
+  screen.debug();
+
+  expect(screen.getByText('Hello World')).toBeInTheDocument();
 });
+
+
